bff2a394646fd20022a214cb8f261d9d9f5275ef
Nikko Ensomo <nikkonuriel@gmail.com>
2026-07-22 14:07:12 +0800

    feat: added initial landing header component

---
+++ client/src/components/headers/LandingPageHeader.tsx
@@ -0,0 +1,33 @@
+import { NavLink } from "react-router-dom";
+
+const LandingPageHeader = () => {
+    return (
+        <>
+            <header className="sticky top-0 z-40 w-full border-b border-gray-200 bg-slate-50 backdrop-blur">
+                <div className="mx-auto w-full flex items-center jusify-between px-12 py-4">
+                    <span className="text-xl font-bold text-zinc-950">Tracky</span>
+
+                    <nav className="hidden gap-6 text-sm font-medium text-gray-600 md:flex">
+                        <NavLink to="/" className="hover:text-slate-900">
+                            Home
+                        </NavLink>
+
+                        <NavLink to="/" className="hover:text-slate-900">
+                            About Us
+                        </NavLink>
+
+                        <NavLink to="/" className="hover:text-slate-900">
+                            Contact Us
+                        </NavLink>
+                    </nav>
+
+                    <div className="flex gap-3">
+                        
+                    </div>
+                </div>
+            </header>
+        </>
+    );
+}
+
+export default LandingPageHeader;
