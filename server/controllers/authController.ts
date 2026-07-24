58504b84ddafad109c5e1160e006b5da586fec71
Nikko Ensomo <nikkonuriel@gmail.com>
2026-07-24 15:11:41 +0800

    feat: added login controller

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -45,3 +45,40 @@ export const signup = async (req: Request, res: Response) => {
         })
     }
 }
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email }).select('+password');
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid credentials.' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials.' });
+        }
+
+        // generate token
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET as string,
+            { expiresIn: "7d" }
+        );
+
+        res.status(200).json({
+            token,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server error.',
+            error: error instanceof Error ? error.message : 'Unknown error.'
+        });
+    }
+}
