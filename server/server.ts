1f50253166d3cfb4c153817be160ac08eca4f9dc
Nikko Ensomo <nikkonuriel@gmail.com>
2026-07-25 15:56:34 +0800

    feat: added get all users controller

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -15,6 +16,7 @@ app.use(express.json());
 
 // routes
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 // server
 const PORT = process.env.PORT || 8080;
