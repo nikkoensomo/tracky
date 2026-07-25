b1382f65a13601f6f730110d3ecd2d06e034d6f0
Nikko Ensomo <nikkonuriel@gmail.com>
2026-07-25 16:16:03 +0800

    feat: added get user by id controller

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -17,3 +17,21 @@ export const getAllUsers = async (req: Request, res: Response) => {
         });
     }
 }
+
+export const getUserById = async (req: Request, res: Response) => {
+    try {
+        const user = await User.findById({ _id: req.params.id })
+
+        if (!user) {
+            return res.status(400).json({ message: 'User does not exist.' });
+        }
+
+        res.status(200).json({ user });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server error.',
+            error: error instanceof Error ? error.message : 'Unknown error.'
+        });
+    }
+}
+
