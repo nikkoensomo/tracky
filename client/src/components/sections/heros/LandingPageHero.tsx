38a35fe9684adfdee6380f203b9478630829eb22
Nikko Ensomo <nikkonuriel@gmail.com>
2026-07-30 22:26:27 +0800

    feat: added new styles in landing page

--- client/src/components/sections/heros/LandingPageHero.tsx
+++ client/src/components/sections/heros/LandingPageHero.tsx
@@ -1,3 +1,4 @@
+import { Check } from 'lucide-react';
 import CTAButtonOne from "../../buttons/CTAButtonOne";
 import CTAButtonTwo from "../../buttons/CTAButtonTwo";
 
@@ -13,12 +14,11 @@ const LandingPageHero = () => {
     return (
         <>
             <section className="flex flex-col gap-6 px-4 py-20 items-center text-center">
-                
                     <h1 className="text-5xl text-slate-950 font-bold max-w-2xl"> 
                         Know where every peso goes - automatically
                     </h1>
 
-                    <p className="text-sm text-slate-500 font-medium max-w-xl">
+                    <p className="text-sm text-slate-500 font-medium max-w-xl mb-4">
                         Track expenses, set budgets, and hit your savings goals — all in one simple dashboard.
                         No spreadsheets required.
                     </p>
@@ -30,6 +30,23 @@ const LandingPageHero = () => {
                         <CTAButtonTwo 
                             onClick={handleSee}
                         />
+                    </div>
+
+                    <div className="flex justify-between gap-3">
+                        <div className="flex gap-2">
+                            <Check size={20}/>
+                            <span className="text-slate-500 text-sm">Free to get started</span>
+                        </div>
+
+                        <div className="flex gap-2">
+                            <Check size={20}/>
+                            <span className="text-slate-500 text-sm">No credit card required</span>
+                        </div>
+
+                        <div className="flex gap-2">
+                            <Check size={20}/>
+                            <span className="text-slate-500 text-sm">Your data stays private</span>
+                        </div>
                     </div>
 
             </section>
