aed56202fc7db12e099a08e869bddc47223b8f8b
Nikko Ensomo <nikkonuriel@gmail.com>
2026-08-08 12:58:28 +0800

    refactor: updated Transaction type

--- client/src/components/sections/heroes/DashboardPageHero.tsx
+++ client/src/components/sections/heroes/DashboardPageHero.tsx
@@ -7,7 +7,7 @@ const DashboardPageHero = () => {
     return (
         <>
             <section className="flex flex-col gap-4">
-                <div className="flex justify-between bg-teal-800 rounded-lg px-4 py-6">
+                <div className="flex justify-between bg-linear-to-r from-teal-800 to-emerald-500 rounded-lg px-4 py-6">
                     <div className="flex flex-col">
                         <span className="text-lg text-gray-300 font-medium">Total Balance</span>
                         <span className="text-3xl text-white font-mono">320,032.03</span>
