aa8f92933aad3fc52094fb8d528025147ad8bd95
Nikko Ensomo <nikkonuriel@gmail.com>
2026-08-13 18:03:49 +0800

    feat: added tiny message

--- client/src/components/headers/DashboardPageHeader.tsx
+++ client/src/components/headers/DashboardPageHeader.tsx
@@ -3,12 +3,14 @@ import SearchBar from "../search/Searchbar";
 const DashboardPageHeader = () => {
     return (
         <>
-            <header className="w-full flex items-center bg-white px-6 py-4 mt-2">
+            <header className="w-full flex items-center justify-between bg-white px-6 py-4 mt-2">
                 <SearchBar
                     value=""
                     onChange={() => { }}
                     placeholder="Search"
                 />
+
+                <span className="text-slate-700 font-semibold">Hola Cretsyy!</span>
             </header>
         </>
     )
