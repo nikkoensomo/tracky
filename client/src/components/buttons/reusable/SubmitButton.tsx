349577be01b3dd34a731f1eb9955c59736021267
Nikko Ensomo <nikkonuriel@gmail.com>
2026-08-16 17:09:06 +0800

    feat: added loading state in creating accounts

--- client/src/components/buttons/reusable/SubmitButton.tsx
+++ client/src/components/buttons/reusable/SubmitButton.tsx
@@ -1,6 +1,6 @@
 type ButtonProps = {
     onClick: () => void;
-    label?: string;
+    label?: any;
 }
 
 const CreateButton = ({ onClick, label }: ButtonProps ) => {
@@ -8,7 +8,7 @@ const CreateButton = ({ onClick, label }: ButtonProps ) => {
         <>
             <button
                 type="button"
-                className="text-white bg-teal-700 hover:bg-teal-800 px-4 py-2 cursor-pointer rounded-lg"
+                className="text-white bg-teal-700 hover:bg-teal-800 px-4 py-2 cursor-pointer rounded-lg disabled:opacity-60 disabled:cursor-not-allowed flex items-center justify-center"
                 onClick={onClick}
             >
                 {label ? label : "Create"}
